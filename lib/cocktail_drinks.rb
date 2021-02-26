074b1ead681903ce12f87ad2f2b8b7bc73b67a06
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-26 12:46:00 -0800

    initial commit

---
+++ lib/cocktail_drinks.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cocktail_drinks/version"
+
+module CocktailDrinks
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cocktail_drinks/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CocktailDrinks
+  VERSION = "0.1.0"
+end
